6720872f4754748182923c4b7fec9546c99e848a
Jairo Lopez <jaloare@gmail.com>
2017-07-20 18:13:52 -0500

    FIX url repo

--- jenkins/jobs/dsl/environment_provisioning.groovy
+++ jenkins/jobs/dsl/environment_provisioning.groovy
@@ -3,7 +3,7 @@ def workspaceFolderName = "${WORKSPACE_NAME}"
 def projectFolderName = "${PROJECT_NAME}"
 
 // Variables
-def environmentTemplateGitUrl = "ssh://jenkins@gerrit:29418/${PROJECT_NAME}/adop-cartridge-java-environment-template"
+def environmentTemplateGitUrl = "https://github.com/Accenture/adop-cartridge-java-environment-template.git"
 
 // Jobs
 def environmentProvisioningPipelineView = buildPipelineView(projectFolderName + "/Environment_Provisioning")
